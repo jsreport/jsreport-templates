b2fbf080184aa5e0b6821b34e36d0f73bec296b6
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2014-10-15 12:54:32 +0200

    embedding initial commit

--- extension/templates/public/js/main_built.js
+++ extension/templates/public/js/main_built.js
@@ -154,7 +154,7 @@ define('template.detail.view',["jquery", "app", "core/utils", "core/view.base", 
             },
 
             events: {
-                "click #previewPane": "triggerPreview"
+                "click #previewPane": "triggerPreview",
             },
 
             onDomRefresh: function() {
@@ -226,7 +226,7 @@ define('template.detail.view',["jquery", "app", "core/utils", "core/view.base", 
 
             validateLeaving: function() {
                 return !this.model.hasChangesSyncLastSync();
-            }
+            },
         });
     });
 /*! 
@@ -286,111 +286,13 @@ define('template.embed.dialog',["marionette", "app", "core/view.base"], function
         }
     });
 });
-define('preview',["underscore", "jquery", "app"], function (_, $, app) {
-
-    function getUIState(model) {
-
-        function justNotNull(o) {
-            var clone = {};
-            for (var key in o) {
-                if (o[key] != null)
-                    clone[key] = o[key];
-            }
-
-            return clone;
-        }
-
-        var state = {};
-        var json = model.toJSON();
-        for (var key in json) {
-            if (json[key] != null) {
-                if (json[key].initData != null)
-                    state[key] = justNotNull(json[key].toJSON());
-                else
-                    state[key] = json[key];
-            }
-        }
-
-        state.content = state.content || " ";
-        state.helpers = state.helpers || "";
-
-        delete state._id;
-        delete state.shortid;
-        return state;
-    }
-
-    function addInput(form, name, value) {
-        var input = document.createElement("input");
-        input.type = "hidden";
-        input.name = name;
-        input.value = value;
-        form.appendChild(input);
-    }
-
-    var fn = function (model, beforeRenderListeners, target) {
-        //this.contentView.$el.find(".preview-loader").show();
-        //http://connect.microsoft.com/IE/feedback/details/809377/ie-11-load-event-doesnt-fired-for-pdf-in-iframe
-        //this.contentView.$el.find("[name=previewFrame]").hide();
-
-        var uiState = getUIState(model);
-
-        var request = { template: uiState };
-
-        beforeRenderListeners.fire(request, function (er) {
-            if (er) {
-                //self.contentView.$el.find(".preview-loader").hide();
-                app.trigger("error", { responseText: er });
-                return;
-            }
-
-            if (uiState.recipe === "client-html") {
-                return app.clientHtml(request, target);
-            }
-
-            var mapForm = document.createElement("form");
-            mapForm.target = target;
-            mapForm.method = "POST";
-            mapForm.action = app.serverUrl + "api/report";
-
-            function addBody(path, body) {
-                if (body == null)
-                    return;
-
-                if (body.initData != null)
-                    body = body.initData;
-
-                for (var key in body) {
-                    if (_.isObject(body[key])) {
-                        addBody(path + "[" + key + "]", body[key]);
-                    } else {
-                        addInput(mapForm, path + "[" + key + "]", body[key]);
-                    }
-                }
-            }
-
-            addBody("template", uiState);
-            if (request.options != null)
-                addBody("options", request.options);
-
-            if (request.data != null)
-                addInput(mapForm, "data", request.data);
-
-            document.body.appendChild(mapForm);
-            mapForm.submit();
-            app.trigger("after-template-render");
-        });
-    }
-
-    return fn;
-});
-
 /*! 
  * Copyright(c) 2014 Jan Blaha 
  */ 
 
 define('template.detail.toolbar.view',["jquery", "app", "core/utils", "core/view.base", "underscore", "core/listenerCollection",
-        "template.embed.dialog", "core/basicModel", "preview"],
-    function($, app, Utils, LayoutBase, _, ListenerCollection, EmbedDialog, BasicModel, preview) {
+        "template.embed.dialog", "core/basicModel"],
+    function($, app, Utils, LayoutBase, _, ListenerCollection, EmbedDialog, BasicModel) {
         return LayoutBase.extend({
             template: "template-detail-toolbar",
             introTemplate: "template-detail-intro",
@@ -478,6 +380,14 @@ define('template.detail.toolbar.view',["jquery", "app", "core/utils", "core/view
                 });
             },
 
+            addInput: function(form, name, value) {
+                var input = document.createElement("input");
+                input.type = "hidden";
+                input.name = name;
+                input.value = value;
+                form.appendChild(input);
+            },
+
             previewNewPanel: function() {
                 this._preview("_blank");
                 this.contentView.$el.find(".preview-loader").hide();
@@ -488,7 +398,84 @@ define('template.detail.toolbar.view',["jquery", "app", "core/utils", "core/view
             },
 
             _preview: function(target) {
-                preview(this.model, this.beforeRenderListeners, target);
+                this.contentView.$el.find(".preview-loader").show();
+                //http://connect.microsoft.com/IE/feedback/details/809377/ie-11-load-event-doesnt-fired-for-pdf-in-iframe
+                //this.contentView.$el.find("[name=previewFrame]").hide();
+
+                var mapForm = document.createElement("form");
+                mapForm.target = target;
+                mapForm.method = "POST";
+                mapForm.action = app.serverUrl + "api/report";
+
+                var uiState = this.getUIState();
+
+                var request = { template: uiState };
+                var self = this;
+                this.beforeRenderListeners.fire(request, function(er) {
+                    if (er) {
+                        self.contentView.$el.find(".preview-loader").hide();
+                        app.trigger("error", { responseText: er });
+                        return;
+                    }
+
+                    function addBody(path, body) {
+                        if (body == null)
+                            return;
+
+                        if (body.initData != null)
+                            body = body.initData;
+
+                        for (var key in body) {
+                            if (_.isObject(body[key])) {
+                                addBody(path + "[" + key + "]", body[key]);
+                            } else {
+                                self.addInput(mapForm, path + "[" + key + "]", body[key]);
+                            }
+                        }
+                    }
+
+                    addBody("template", uiState);
+                    if (request.options != null)
+                        addBody("options", request.options);
+
+                    if (request.data != null)
+                        self.addInput(mapForm, "data", request.data);
+
+                    document.body.appendChild(mapForm);
+                    mapForm.submit();
+                    app.trigger("after-template-render");
+                });
+            },
+
+            getUIState: function() {
+
+                function justNotNull(o) {
+                    var clone = {};
+                    for (var key in o) {
+                        if (o[key] != null)
+                            clone[key] = o[key];
+                    }
+
+                    return clone;
+                }
+
+                var state = {};
+                var json = this.model.toJSON();
+                for (var key in json) {
+                    if (json[key] != null) {
+                        if (json[key].initData != null)
+                            state[key] = justNotNull(json[key].toJSON());
+                        else
+                            state[key] = json[key];
+                    }
+                }
+
+                state.content = state.content || " ";
+                state.helpers = state.helpers || "";
+
+                delete state._id;
+                delete state.shortid;
+                return state;
             },
 
             onValidate: function() {
