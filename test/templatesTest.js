2e040a1dee5e6f7dda774ce4de08118cb7b1cb51
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-11-18 10:17:14 +0100

    fix #1 log preview

--- test/templatesTest.js
+++ test/templatesTest.js
@@ -1,29 +1,29 @@
 var path = require('path')
 var assert = require('assert')
-var Reporter = require('jsreport-core').Reporter
+var JsReport = require('jsreport-core')
 
 describe('templating', function () {
-  var reporter
+  var jsreport
 
   beforeEach(function () {
-    reporter = new Reporter({
+    jsreport = new JsReport({
       rootDirectory: path.join(__dirname, '../')
     })
 
-    return reporter.init()
+    return jsreport.init()
   })
 
   it('handleBefore should find by _id and use template', function () {
     var request = {
       template: {},
-      logger: reporter.logger,
-      context: reporter.context,
+      logger: jsreport.logger,
+      context: jsreport.context,
       options: {recipe: 'html'}
     }
 
-    return reporter.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
+    return jsreport.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
       request.template._id = t._id
-      return reporter.templates.handleBeforeRender(request, {}).then(function () {
+      return jsreport.templates.handleBeforeRender(request, {}).then(function () {
         assert.equal('foo', request.template.content)
       })
     })
@@ -32,14 +32,14 @@ describe('templating', function () {
   it('should callback weak error when missing template', function () {
     var request = {
       template: {_id: '507f191e810c19729de860ea'},
-      logger: reporter.logger,
-      context: reporter.context,
+      logger: jsreport.logger,
+      context: jsreport.context,
       options: {recipe: 'html'}
     }
 
     var response = {}
 
-    return reporter.templates.handleBeforeRender(request, response).catch(function (err) {
+    return jsreport.templates.handleBeforeRender(request, response).catch(function (err) {
       assert.equal(err.weak, true)
     })
   })
@@ -47,14 +47,14 @@ describe('templating', function () {
   it('handleBefore should find by shortid and use template', function () {
     var request = {
       template: {},
-      logger: reporter.logger,
-      context: reporter.context,
+      logger: jsreport.logger,
+      context: jsreport.context,
       options: {recipe: 'html'}
     }
 
-    return reporter.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
+    return jsreport.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
       request.template.shortid = t.shortid
-      return reporter.templates.handleBeforeRender(request, {}).then(function () {
+      return jsreport.templates.handleBeforeRender(request, {}).then(function () {
         assert.equal('foo', request.template.content)
       })
     })
@@ -63,13 +63,13 @@ describe('templating', function () {
   it('handleBefore should find by name and use template', function () {
     var request = {
       template: {name: 'x'},
-      logger: reporter.logger,
-      context: reporter.context,
+      logger: jsreport.logger,
+      context: jsreport.context,
       options: {recipe: 'html'}
     }
 
-    return reporter.documentStore.collection('templates').insert({content: 'foo', name: 'x'}).then(function (t) {
-      return reporter.templates.handleBeforeRender(request, {}).then(function () {
+    return jsreport.documentStore.collection('templates').insert({content: 'foo', name: 'x'}).then(function (t) {
+      return jsreport.templates.handleBeforeRender(request, {}).then(function () {
         assert.equal('foo', request.template.content)
       })
     })
@@ -78,12 +78,12 @@ describe('templating', function () {
   it('handleBefore with content and not existing name should pass', function () {
     var request = {
       template: {name: 'x', content: ' '},
-      logger: reporter.logger,
-      context: reporter.context,
+      logger: jsreport.logger,
+      context: jsreport.context,
       options: {recipe: 'html'}
     }
 
-    return reporter.templates.handleBeforeRender(request, {}).then(function () {
+    return jsreport.templates.handleBeforeRender(request, {}).then(function () {
       assert.equal(' ', request.template.content)
     })
   })
@@ -91,23 +91,23 @@ describe('templating', function () {
   it('handleBefore with not existing template should fail requesting handleBefore second time with existing template should succeed', function () {
     var request = {
       template: {},
-      context: reporter.context,
-      logger: reporter.logger,
+      context: jsreport.context,
+      logger: jsreport.logger,
       options: {recipe: 'html'}
     }
 
-    return reporter.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
+    return jsreport.documentStore.collection('templates').insert({content: 'foo'}).then(function (t) {
       request.template.shortid = 'not existing'
 
-      return reporter.templates.handleBeforeRender(request, {}).catch(function () {
+      return jsreport.templates.handleBeforeRender(request, {}).catch(function () {
         request = {
           template: {shortid: t.shortid},
-          logger: reporter.logger,
-          context: reporter.context,
+          logger: jsreport.logger,
+          context: jsreport.context,
           options: {recipe: 'html'}
         }
 
-        return reporter.templates.handleBeforeRender(request, {}).then(function () {
+        return jsreport.templates.handleBeforeRender(request, {}).then(function () {
           assert.equal('foo', request.template.content)
         })
       })
@@ -117,21 +117,21 @@ describe('templating', function () {
   it('handleBefore should throw when no content and id specified', function () {
     var request = {
       template: {},
-      context: reporter.context,
-      logger: reporter.logger,
+      context: jsreport.context,
+      logger: jsreport.logger,
       options: {recipe: 'html'}
     }
 
     assert.throws(function () {
-      reporter.templates.handleBeforeRender(request, {})
+      jsreport.templates.handleBeforeRender(request, {})
     })
   })
 
   it('deleting should work', function () {
-    return reporter.documentStore.collection('templates').insert({content: 'foo'})
+    return jsreport.documentStore.collection('templates').insert({content: 'foo'})
       .then(function (t) {
-        return reporter.documentStore.collection('templates').remove({shortid: t.shortid}).then(function () {
-          return reporter.documentStore.collection('templates').find({}).then(function (list) {
+        return jsreport.documentStore.collection('templates').remove({shortid: t.shortid}).then(function () {
+          return jsreport.documentStore.collection('templates').find({}).then(function (list) {
             assert.equal(list.length, 0)
           })
         })
